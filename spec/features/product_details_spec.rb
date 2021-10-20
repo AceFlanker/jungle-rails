ae1d25b9ec4902d33c267db06a2862fb0ff55947
Yuan (Daniel) Li <86581841+AceFlanker@users.noreply.github.com>
2021-10-19 21:10:13 -0700

    Uncomment test specs

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,33 +1,33 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.feature "Visitor navigates from home page to a product page", type: :feature, js: true do
+RSpec.feature "Visitor navigates from home page to a product page", type: :feature, js: true do
 
-#   # SETUP
-#   before :each do
-#     @category = Category.create! name: 'Apparel'
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
 
-#     10.times do |n|
-#       @category.products.create!(
-#         name:  Faker::Hipster.sentence(3),
-#         description: Faker::Hipster.paragraph(4),
-#         image: open_asset('apparel1.jpg'),
-#         quantity: 10,
-#         price: 64.99
-#       )
-#     end
-#   end
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
 
-#   scenario "They see all products" do
-#     visit root_path
+  scenario "They see all products" do
+    visit root_path
 
 
 
-#     within(".products article:first-child") do
-#       click_on "Details"
-#     end
+    within(".products article:first-child") do
+      click_on "Details"
+    end
 
-#     expect(page).to have_css '.products-show'
+    expect(page).to have_css '.products-show'
 
-#   end
+  end
   
-# end
+end
